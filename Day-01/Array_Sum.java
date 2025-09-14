bd35a7f4a5d7c854b59d4e311194bac3799cad3e
Anuj-prajapati-SDE <anujprajapa3@gmail.com>
2025-09-14 22:58:01 +0530

    First program push

---
+++ Day-01/Array_Sum.java
@@ -0,0 +1,12 @@
+public class Array_Sum {
+  public static void main(String[] args) {
+     int sum = 0;
+        int[] arr = {1, 2, 3, 4, 5};
+
+       for(int i = 0 ; i<arr.length ; i++){
+         sum = sum + arr[i];
+       }
+       System.out.println("Sum of array elements: " + sum);
+  }
+    
+}
