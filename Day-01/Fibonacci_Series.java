f010b6e007a8ed87f8bf361bec4ff7bf22e104c6
Anuj-prajapati-SDE <anujprajapa3@gmail.com>
2025-09-15 23:33:52 +0530

    modify code

--- Day-01/Fibonacci_Series.java
+++ Day-01/Fibonacci_Series.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 public class Fibonacci_Series {
 public static void main(String[] args) {
@@ -6,6 +5,12 @@ public static void main(String[] args) {
     int b = 1; 
     Scanner readElement = new Scanner(System.in);
     int  inputElement  = readElement.nextInt();
-
+    System.out.print(a + " " + b);
+    for(int i=2; i<inputElement; i++){
+        int c = a + b;
+        System.out.print(" " + c);
+        a = b;
+        b = c;
+    }
 }
 }
