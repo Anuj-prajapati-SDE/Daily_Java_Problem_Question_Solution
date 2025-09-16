1c964b1790f4a313a322100d53f59f3544257b97
Anuj-prajapati-SDE <anujprajapa3@gmail.com>
2025-09-16 23:36:48 +0530

    Add Colum_Matrix_Print class to print a matrix in column format

---
+++ Day-02/Colum_Matrix_Print.java
@@ -0,0 +1,17 @@
+public class Colum_Matrix_Print {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},            
+            {7, 8, 9},            
+        };
+
+     for(int i = 0; i<matrix.length; i++){
+        for(int j = 0 ; j<matrix[i].length; j++){ 
+              System.out.print(matrix[j][i] + " ");
+        }
+        System.out.println();
+     }
+    }
+       
+}
